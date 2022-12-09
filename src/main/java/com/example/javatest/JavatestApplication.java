5bc15424d276c154e18172a2aca0d78ca9f08999
yumble <hnjn1101@gmail.com>
2022-12-09 11:22:22 +0900

    feat : solve problem3

--- src/main/java/com/example/javatest/JavatestApplication.java
+++ src/main/java/com/example/javatest/JavatestApplication.java
@@ -1,13 +1,21 @@
 package com.example.javatest;
 
+import com.example.javatest.subset.Problem3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 @SpringBootApplication
 public class JavatestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavatestApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        //SpringApplication.run(JavatestApplication.class, args);
+        Problem3 problem3 = new Problem3();
+        problem3.solve();
     }
 
 }
