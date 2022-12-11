6c7f0acf9395f60ab4a3353a7624d49e56cc6aee
yumble <hnjn1101@gmail.com>
2022-12-11 14:42:47 +0900

    feat : solve problem5

--- src/main/java/com/example/javatest/JavatestApplication.java
+++ src/main/java/com/example/javatest/JavatestApplication.java
@@ -2,6 +2,7 @@ package com.example.javatest;
 
 import com.example.javatest.subset.Problem3;
 import com.example.javatest.subset.Problem4;
+import com.example.javatest.subset.Problem5;
 
 import java.io.IOException;
 
@@ -9,8 +10,8 @@ public class JavatestApplication {
 
     public static void main(String[] args) throws IOException {
         //SpringApplication.run(JavatestApplication.class, args);
-        Problem4 problem4 = new Problem4();
-        problem4.solve();
+        Problem5 problem = new Problem5();
+        problem.solve();
     }
 
 }
